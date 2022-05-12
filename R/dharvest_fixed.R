243dfc4885b731cb0355c17c586cdff56547f192
Mia Forsline <miaforsline@gmail.com>
2022-05-12 10:24:14 -0700

    in class pop growth stability demo

--- R/dharvest_fixed.R
+++ R/dharvest_fixed.R
@@ -1,4 +1,4 @@
-#'  Logistic population growth derivative with harvesting
+#'  Logistic population growth derivative with harvesting - fixed harvest 
 #' @param time time since start
 #' @param P population
 #' @param parms - as list with three values, r, K, harv
@@ -7,8 +7,12 @@
 #' @param h harvest rate
 #' @return derivative of population with time 
 
-dharvest= function(Time, P, parms) {
-	
-	dP = parms$r * P * (1- P/parms$K) - parms$harv*P
-	return(list(dP))
+dharvest_fixed= function(Time, P, parms) {
+  
+  min_C = 0
+  
+  if(parms$harv < min_C) {"error"}
+  
+  dP = parms$r * P * (1- P/parms$K) - parms$harv
+  return(list(dP))
 }
